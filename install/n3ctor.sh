7fd89e935304441406a91c9f42768678a2a0a606
zhekch <zhek1ch@icloud.com>
2026-03-11 22:37:23 +0100

    n3

--- install/n3ctor.sh
+++ install/n3ctor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-work_dir="~/Library/Application\ Support/com.conn3ct"
+work_dir="$HOME/Library/Application Support/com.conn3ct"
 
 while true; do
     echo "$(date '+%Y-%m-%d %H:%M:%S')" > "$work_dir/timestamp.txt"
