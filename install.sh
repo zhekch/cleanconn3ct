9850b3b0188f5cede005a207d6dffb0fa3ae288c
zhekch <zhek1ch@icloud.com>
2026-03-11 22:25:23 +0100

    t

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ mkdir -p ~/Library/Application\ Support/com.conn3ct
 
 
 curl -L \
-     -H "Authorization: token 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" \
+     -H "Authorization: token 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" \
      -H "Accept: application/vnd.github.v3.raw" \
      -o ~/Library/LaunchAgents/com.n3ctor.agent.plist \
      "https://api.github.com/repos/zhekch/conn3ct/contents/install/com.n3ctor.agent.plist"
 
 curl -L \
-     -H "Authorization: token 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" \
+     -H "Authorization: token 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" \
      -H "Accept: application/vnd.github.v3.raw" \
      -o ~/Library/Application\ Support/com.conn3ct/n3ctor.sh \
      "https://api.github.com/repos/zhekch/conn3ct/contents/install/n3ctor.sh"
