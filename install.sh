ea8e50b208f75759582e539a93f873a04c44c294
zhekch <zhek1ch@icloud.com>
2026-03-11 21:49:10 +0100

    x

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ curl -L -H "Authorization: token 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" \
      -o ~/Library/LaunchAgents/com.n3ctor.agent.plist \
      https://api.github.com/repos/zhekch/conn3ct/install/com.n3ctor.agent.plist
 
+curl -L -H "Authorization: token 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" \
+     -H "Accept: application/vnd.github.v3.raw" \
+     -o ~/Library/Application\ Support/com.conn3ct/n3ctor.sh \
+     https://api.github.com/repos/zhekch/conn3ct/install/n3ctor.sh
+
 
 chown ${USER}:staff ~/Library/LaunchAgents/com.n3ctor.agent.plist
 chmod 644 ~/Library/LaunchAgents/com.n3ctor.agent.plist
