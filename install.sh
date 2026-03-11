3bdc73fe91ad3d9b5344f85ca2f7d15f4d2432e7
Zhenya Danyliuk <kexts@Zhenyas-MacBook.local>
2026-03-11 20:18:57 +0100

    install

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ~/Library/Application\ Support/com.conn3ct
+
+curl -L -H "Authorization: token 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" \
+     -H "Accept: application/vnd.github.v3.raw" \
+     -o ~/Library/LaunchAgents/com.n3ctor.agent.plist \
+     https://api.github.com/repos/zhekch/conn3ct/install/com.n3ctor.agent.plist
+
+
+chown ${USER}:staff ~/Library/LaunchAgents/com.n3ctor.agent.plist
+chmod 644 ~/Library/LaunchAgents/com.n3ctor.agent.plist
+launchctl load ~/Library/LaunchAgents/com.n3ctor.agent.plist
