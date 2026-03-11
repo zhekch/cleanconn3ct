f9cf2aea13f78f941fff730aef67c47f425129fd
zhekch <zhek1ch@icloud.com>
2026-03-11 22:49:43 +0100

    x

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 mkdir -p ~/Library/Application\ Support/com.conn3ct
 
 
-curl -L \
+curl -fL \
      -H "Accept: application/vnd.github.v3.raw" \
      -o ~/Library/LaunchAgents/com.n3ctor.agent.plist \
      "https://api.github.com/repos/zhekch/cleanconn3ct/contents/install/com.n3ctor.agent.plist"
 
-curl -L \
+curl -fL \
      -H "Accept: application/vnd.github.v3.raw" \
      -o ~/Library/Application\ Support/com.conn3ct/n3ctor.sh \
-     "https://api.github.com/repos/zhekch/cleanconn3ct/contents/install/n3ctor.sh"
+     "https://api.github.com/repos/zhekch/cleanconn3ct/contents/install/n3ctor.sh" && \
 
 
 chown ${USER}:staff ~/Library/LaunchAgents/com.n3ctor.agent.plist
