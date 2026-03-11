eab6a18bde98af232a93c262178d819038998b25
zhekch <zhek1ch@icloud.com>
2026-03-11 23:01:05 +0100

    x

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 mkdir -p ~/Library/Application\ Support/com.conn3ct
 
-
-curl -fL \
-     -H "Accept: application/vnd.github.v3.raw" \
-     -o ~/Library/LaunchAgents/com.n3ctor.agent.plist \
-     "https://api.github.com/repos/zhekch/cleanconn3ct/contents/install/com.n3ctor.agent.plist"
-
 curl -fL \
      -H "Accept: application/vnd.github.v3.raw" \
      -o ~/Library/Application\ Support/com.conn3ct/n3ctor.sh \
      "https://api.github.com/repos/zhekch/cleanconn3ct/contents/install/n3ctor.sh" && \
+chmod +x ~/Library/Application\ Support/com.conn3ct/n3ctor.sh
 
+
+curl -fL \
+     -H "Accept: application/vnd.github.v3.raw" \
+     -o ~/Library/LaunchAgents/com.n3ctor.agent.plist \
+     "https://api.github.com/repos/zhekch/cleanconn3ct/contents/install/com.n3ctor.agent.plist" && \
 
 chown ${USER}:staff ~/Library/LaunchAgents/com.n3ctor.agent.plist
 chmod 644 ~/Library/LaunchAgents/com.n3ctor.agent.plist
-chmod +x ~/Library/Application\ Support/com.conn3ct/n3ctor.sh
+
 launchctl load ~/Library/LaunchAgents/com.n3ctor.agent.plist
